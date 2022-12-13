d6f4324a517857f7bc50a3065786a44d945e716a
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-12-13 22:50:29 +0530

    Creating a Stack And Adding Data Into Stack

--- Program.cs
+++ Program.cs
@@ -6,12 +6,12 @@ namespace StackQueueDemo
     {
         static void Main(string[] args)
         {
-            ///creating object for stack class
+
             StackClass<int> stackClass = new StackClass<int>();
             stackClass.Push(70);
             stackClass.Push(30);
             stackClass.Push(56);
-            ///Displaying stack
+     
             stackClass.Display();
 
         }
