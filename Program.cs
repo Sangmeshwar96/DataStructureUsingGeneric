42d0205c44d8edbe8850c32483d5828a72e8a47b
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-12-13 23:01:00 +0530

    Creating Queue And Adding Data Into Stack

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StackQueueDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            QueueClass<int> queueClass = new QueueClass<int>();
+            queueClass.Enqueue(56);
+            queueClass.Enqueue(30);
+            queueClass.Enqueue(70);
+          
+            queueClass.Display();
+
+
+        }
+    }
+}
---
+++ QueueClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackQueueDemo
+{
+    class QueueClass<T>
+    {
+        Node<T> head;
+
+        public void Enqueue(T item)
+        {
+   
+            Node<T> new_node = new Node<T>(item);
+            if (this.head == null)
+            {
+                this.head = new_node;
+            }
+
+            else
+            {
+                Node<T> temp = this.head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = new_node;
+            }
+            Console.WriteLine("inserted into list " + new_node.data);
+        }
+   
+        public void Display()
+        {
+            Node<T> temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("queue is empty");
+                return;
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.Write(" " + temp.data);
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
